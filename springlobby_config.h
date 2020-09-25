c57d4b0e30622df3c7472a3bff4256b8f3c8632c
flower-spring <flower-spring@users.noreply.github.com>
2020-09-25 19:01:42 +0200

    for version

---
+++ springlobby_config.h
@@ -0,0 +1,11 @@
+/* This file is part of the Springlobby (GPL v2 or later), see COPYING */
+
+#ifndef SPRINGLOBBY_HEADERGUARD_CONFIG_H
+#define SPRINGLOBBY_HEADERGUARD_CONFIG_H
+
+#undef VERSION
+
+/* the git tag / commit we build from */
+#define VERSION "0.268-alternative"
+
+#endif	/* SPRINGLOBBY_HEADERGUARD_CONFIG_H */
